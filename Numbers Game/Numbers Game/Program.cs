e84d4a04f9430693bb6957cedd00673261f4bd23
Meggan <meggtriplett@gmail.com>
2019-06-11 11:44:08 -0700

    Populate method fills array and returns to StartSequence

--- Numbers Game/Numbers Game/Program.cs
+++ Numbers Game/Numbers Game/Program.cs
@@ -25,28 +25,43 @@ namespace Numbers_Game
 
         static void StartSequence()
         {
+            Console.WriteLine("Please enter a number greater than zero. Keep in mind you will have to enter this amount of numbers afterwards.");
+            string userGivenString = Console.ReadLine();
+            int givenNumber = Convert.ToInt32(userGivenString);
 
+            int[] userArray = new int[givenNumber];
+
+            Populate(userArray);
         }
 
         static int[] Populate(int[] arr)
         {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"Please enter a number: {i + 1} out of {arr.Length}.");
+                string arrString = Console.ReadLine();
+                int arrNumber = Convert.ToInt32(arrString);
 
+                arr[i] = arrNumber;
+            }
+
+            return arr;
         }
 
-        static int GetSum(int[] arr)
-        {
+        //static int GetSum(int[] arr)
+        //{
 
-        }
+        //}
 
-        static int GetProduct(int[] arr, int sum)
-        {
+        //static int GetProduct(int[] arr, int sum)
+        //{
 
-        }
+        //}
 
-        static decimal GetQuotient(int product)
-        {
+        //static decimal GetQuotient(int product)
+        //{
 
-        }
+        //}
 
     }
 }
