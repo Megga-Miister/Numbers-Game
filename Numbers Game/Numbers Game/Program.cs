aa7a519d5555bbab5f5ae9c4d7581d908dcf9f5a
Meggan <meggtriplett@gmail.com>
2019-06-11 11:25:48 -0700

    Main method now calls StartSequence method and has general exception handler.

--- Numbers Game/Numbers Game/Program.cs
+++ Numbers Game/Numbers Game/Program.cs
@@ -7,6 +7,20 @@ namespace Numbers_Game
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to my game! Let's do some math!");
+
+            try
+            {
+                StartSequence();
+            }
+            catch (Exception ge)
+            {
+                Console.WriteLine($"It looks like you have ran into an issue: {ge.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Program is complete.");
+                Console.ReadKey();
+            }
         }
 
         static void StartSequence()
