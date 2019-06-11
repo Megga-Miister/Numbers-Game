4d1ad74faea83fbce55c1b95eb01395f2d6faafa
Meggan <meggtriplett@gmail.com>
2019-06-11 11:17:54 -0700

    Initial method scaffolding

--- Numbers Game/Numbers Game/Program.cs
+++ Numbers Game/Numbers Game/Program.cs
@@ -6,7 +6,33 @@ namespace Numbers_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to my game! Let's do some math!");
         }
+
+        static void StartSequence()
+        {
+
+        }
+
+        static int[] Populate(int[] arr)
+        {
+
+        }
+
+        static int GetSum(int[] arr)
+        {
+
+        }
+
+        static int GetProduct(int[] arr, int sum)
+        {
+
+        }
+
+        static decimal GetQuotient(int product)
+        {
+
+        }
+
     }
 }
